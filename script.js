3b94724f158b38723ba1ed215bd372d77639ce54
xiaotian <952822399@qq.com>
2017-07-19 15:17:31 +0800

    1.0

--- script.js
+++ script.js
@@ -30,28 +30,13 @@ orightSubmit.onclick = function(){
 	location.href="http://www.baidu.com/s?wd="+oleftSearch.value+"";
 }
 oleftSearch.onfocus = function(){
-	console.log(1);
-	if (this.value != '') {
-		var oScript = document.createElement('script');
-		oScript.src='http://suggestion.baidu.com/su?wd='+this.value+'&cb=tian';
-		document.body.appendChild(oScript);
+	if (oleftSearch.value != '') {
+		oleftSearchUlParent.style.display = 'block';
 	} else {
 		oleftSearchUlParent.style.display = 'none';
 	}
 
-	function tian(data){
-	var html = '';
-	if (data.s.length) {
-		oleftSearchUlParent.style.display = 'block';
-		for (var i = 0; i < data.s.length; i++) {
-			html += '<li><a target="_blank" href="http://www.baidu.com/s?wd='+data.s[i]+'">'+ data.s[i] +'</a></li>';
-		}
-		oleftSearchUl.innerHTML = html;
-	} else {
-		oleftSearchUlParent.style.display = 'none';
-	}
-	}
 }
-oleftSearch.onblur =function(){
-	oleftSearchUlParent.style.display = 'none';
-}
+// oleftSearch.onblur =function(){
+// 	oleftSearchUlParent.style.display = 'none';
+// }
